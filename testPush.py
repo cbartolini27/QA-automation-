1fec1d9f979854f3dd83c3e6ead3c7d034e3f805
Christian Bartolini <christian.bartolini99@gmail.com>
2024-09-29 22:34:39 -0400

    Able to access google search bar, type Moo dang and press enter.

--- testPush.py
+++ testPush.py
@@ -1,14 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.common.keys import Keys #In order to do something like press enter, arrow keys or anything where we can't type
+import time
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
+driver.get("https://google.com") #Opens a page or a website
 
-print("Hello World")
+search_bar = driver.find_element(By.CLASS_NAME, "gLFyf") #This allows you to get the element you're working, this example we chose the class identifier for the search bar on google
+search_bar.send_keys("Moo dang" + Keys.ENTER) #This allows you to type into the element
 
-#Testing single line with python rn
-'''
-Just testing multilined with python rn
-'''
+time.sleep(10)
+
+driver.quit()
+
